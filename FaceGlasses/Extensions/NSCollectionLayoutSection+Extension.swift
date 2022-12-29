36693e88d4e0e81db0281bb67d49cd40e5750293
Shandor Balogh <shandor.balogh@coaxsoft.com>
2022-12-29 16:09:10 +0100

    Removed unnecessary сщву

--- FaceGlasses/Extensions/NSCollectionLayoutSection+Extension.swift
+++ FaceGlasses/Extensions/NSCollectionLayoutSection+Extension.swift
@@ -7,17 +7,17 @@
 
 import UIKit
 
-extension NSCollectionLayoutSection {
-    static let collectionLayoutSection: NSCollectionLayoutSection = {
-        let contentInset: CGFloat = 5
-        let groupWidthHeight: CGFloat = 80
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = .init(top: contentInset, leading: contentInset, bottom: contentInset, trailing: contentInset)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(groupWidthHeight), heightDimension: .estimated(groupWidthHeight))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .continuous
-        return section
-    }()
-}
+//extension NSCollectionLayoutSection {
+//    static let collectionLayoutSection: NSCollectionLayoutSection = {
+//        let contentInset: CGFloat = 5
+//        let groupWidthHeight: CGFloat = 80
+//        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+//        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//        item.contentInsets = .init(top: contentInset, leading: contentInset, bottom: contentInset, trailing: contentInset)
+//        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(groupWidthHeight), heightDimension: .estimated(groupWidthHeight))
+//        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+//        let section = NSCollectionLayoutSection(group: group)
+//        section.orthogonalScrollingBehavior = .continuous
+//        return section
+//    }()
+//}
